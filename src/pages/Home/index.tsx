939fa5baba180dc737991730bb912ff9795e8df8
gankken <kokken95@gmail.com>
2023-03-16 21:54:09 +0800

    Change page title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,8 @@
-import { Helmet as Head } from 'react-helmet-async';
 import App from 'App';
 
 const HomePage = (): JSX.Element => {
   return (
     <>
-      <Head>
-        <title>Naluri Web - Chat</title>
-      </Head>
       <App />
     </>
   );
